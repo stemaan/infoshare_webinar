7963b074cdb4350a31d15c445fe058e4677962bc
Przemyslaw Lalak <lalak@protonmail.com>
2018-08-10 14:32:34 +0200

    add requirements

--- overpass_example.py
+++ overpass_example.py
@@ -2,6 +2,7 @@ import json
 import overpy
 import folium
 
+
 with open('./route.json', encoding='utf-8') as geojson:
     loaded_data = json.load(geojson)
     geometry = loaded_data.get('geometry')
@@ -12,23 +13,26 @@ tag = 'fuel'
 query_template = '(node["amenity"="{tag}"](around:{radius},{lat},{lon}););out body;'
 
 # get only latitude and longitude, no need for elevation
-geometry = [(lat, long) for lat, long, alt in geometry]
+geometry = [(lat, lon) for lat, lon, alt in geometry]
 middle_idx = len(geometry) // 2
 middle_point = geometry[middle_idx]
 
 my_map = folium.Map(location=middle_point, zoom_start=10)
 
+# draw route on the map
 folium.PolyLine(geometry).add_to(my_map)
 
-for latitude, longitude in geometry[::250]:
+# each GPS point is stored every 50 meters, let's search POI for every 200th point (~10km)
+for latitude, longitude in geometry[::200]:
     result = api.query(
         query_template.format(
             tag=tag, radius=radius, lat=latitude, lon=longitude
         )
     )
 
+    # place found fuel stations as markers on the map
     for node in result.nodes:
         folium.map.Marker([node.lat, node.lon],
                           popup=node.tags.get('brand', tag)).add_to(my_map)
 
-my_map.save('mapa3.html')
+my_map.save('map.html')
